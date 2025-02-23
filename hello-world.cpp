a8fc43141bcf58040382052456952d8998c94dfa
100daysofredteam <100daysofredteam@gmail.com>
2025-02-23 13:09:14 +0530

    Create hello-world.cpp

---
+++ hello-world.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <stdio.h>
+
+extern "C" {
+
+#include "beacon.h"
+
+int go(char * args, unsigned long length) {
+   BeaconPrintf(CALLBACK_OUTPUT, "This BOF was compiled as C++ file. Thank you for joining me on 100 Days of Red Team\n");
+   return 0;
+}
+
+}
